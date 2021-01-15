8a5ea391df0d450fddf1204d38772ef77e105680
nsoto0216@gmail.com <nsoto0216@gmail.com>
2021-01-15 16:19:54 -0500

    changed python version

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+# basic function to test unittesting.
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+def sub(x, y):
+    """Subtract two numbers together"""
+    return x - y
