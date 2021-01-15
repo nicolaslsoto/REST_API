a4ba545b9347ae68fe1d2f86f485db514d901107
nsoto0216@gmail.com <nsoto0216@gmail.com>
2021-01-15 17:58:57 -0500

    fixed issue where flake8 was causing build issues, added core

--- app/app/calc.py
+++
@@ -1,9 +0,0 @@
-# basic function to test unittesting.
-
-def add(x, y):
-    """Add two numbers together"""
-    return x + y
-
-def sub(x, y):
-    """Subtract two numbers together"""
-    return x - y
